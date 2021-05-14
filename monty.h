321b281137f1e7ed29ffbbb0c7cc1e51410a6667
Cristian Pinzón <faykris28@gmail.com>
2021-05-13 23:59:51 -0500

    Revert "add mod opcode i opcodes, header and c file"
    This reverts commit 91929f03ddec5e49d73bf9aea0bb60e9e65f6a6f.

--- monty.h
+++ monty.h
@@ -70,6 +70,5 @@ void _add(stack_t **stack, unsigned int lines);
 void _sub(stack_t **stack, unsigned int lines);
 void _div(stack_t **stack, unsigned int lines);
 void _mul(stack_t **stack, unsigned int lines);
-void _mod(stack_t **stack, unsigned int lines);
 
 #endif
--- valid_opcodes.c
+++ valid_opcodes.c
@@ -17,7 +17,7 @@ void valid_opcodes(char *buffer, unsigned int lines, stack_t **stack)
 		{"push", _push}, {"pall", _pall}, {"pint", _pint},
 		{"swap", _swap}, {"nop", _nop}, {"add", _add},
 		{"pop", _pop}, {"sub", _sub}, {"div", _div},
-		{"mul", _mul}, {"mod", _mod}, {NULL, NULL}
+		{"mul", _mul}, {NULL, NULL}
 	};
 
 	token1 = strtok(buffer, delimiter);

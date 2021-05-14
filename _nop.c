cb6533a7be98152f02c35bbd9e556581dadef924
AnguiDev <angiedesarrollo@gmail.com>
2021-05-13 19:52:42 -0500

    function that do nothing

---
+++ _nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * _nop - Function that fo nothing
+ * @stack: Elements past for stack
+ * @lines: Lines of read past bytecodes
+ * Return: None.
+ **/
+void _nop(stack_t **stack, unsigned int lines)
+{
+	(void)stack;
+	(void)lines;
+}

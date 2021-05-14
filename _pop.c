8edf60061ed3a0b79d63933038d01c300c7df021
Cristian Pinzón <faykris28@gmail.com>
2021-05-13 22:10:37 -0500

    fix pop function - SF when pop element when the stack is 1 element

--- _pop.c
+++ _pop.c
@@ -11,7 +11,7 @@ void _pop(stack_t **stack, unsigned int lines)
 
 	if (*stack == NULL)
 	{
-		printf("L%d: can't pop an empty stack", lines);
+		printf("L%d: can't pop an empty stack\n", lines);
 		free(f_struct.buffer);
 		free_stack(stack);
 		fclose(f_struct.fp);
@@ -21,8 +21,6 @@ void _pop(stack_t **stack, unsigned int lines)
 	{
 		tmp = *stack;
 		*stack = (*stack)->prev;
-		(*stack)->next = NULL;
-
 		free(tmp);
 	}
 }
